257e1dfd7f4dcc4374e2689de55e4414de9175f7
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2019-09-02 16:19:14 +0200

    new cmdlet

---
+++ SwissKnife/Public/Get-ObjectMember.ps1
@@ -0,0 +1,34 @@
+﻿function Get-ObjectMember {
+    <#
+    .SYNOPSIS
+    Helper to turn PSCustomObject into a list of key/value pairs
+
+    .DESCRIPTION
+    Helper to turn PSCustomObject into a list of key/value pairs
+
+    .PARAMETER Object
+    PSCustomObject
+
+    .EXAMPLE
+    Invoke-RestMethod -Uri 'https://metadata.wayf.dk/idps.js' | Get-ObjectMember | ForEach-Object {
+        $_.Value
+    }
+
+    .NOTES
+    Author: Tomalak
+
+    .LINK
+    https://stackoverflow.com/a/33521853
+    #>
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true)]
+        [PSCustomObject]$Object
+    )
+    $Object | Get-Member -MemberType NoteProperty | ForEach-Object {
+        $Key = $_.Name
+        [PSCustomObject]@{Key = $Key; Value = $Object."$Key" }
+    }
+}
---
+++ SwissKnife/Public/Get-WAYFMetadata.ps1
@@ -0,0 +1,33 @@
+﻿function Get-WAYFMetadata {
+    <#
+    .SYNOPSIS
+    List WAYF metadata.
+
+    .DESCRIPTION
+    Long description
+
+    .EXAMPLE
+    JSON summary of IdPs showing their schacHomeOrganization values.
+
+    .NOTES
+    Author: Frederik Hjorslev Poulsen
+    #>
+
+    [CmdletBinding()]
+    param (
+    )
+
+    begin {
+        $Metadata = @()
+    }
+
+    process {
+        Invoke-RestMethod -Uri 'https://metadata.wayf.dk/idps.js' | Get-ObjectMember | ForEach-Object {
+            $Metadata += $_.Value
+        }
+    }
+
+    end {
+        Write-Output -InputObject $Metadata | Select-Object -Property schacHomeOrganization, da, en | Sort-Object -Property schacHomeOrganization -Unique
+    }
+}
