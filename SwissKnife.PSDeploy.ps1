7bc2ff8f66b460586e4d3f49184092c7aa142898
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2019-09-28 22:38:06 +0200

    !deploy !ver:MINOR

--- SwissKnife.PSDeploy.ps1
+++
@@ -1,9 +0,0 @@
-﻿# Publish to AppVeyor if we're in AppVeyor
-if ($env:BHProjectName -and $env:BHProjectName.Count -eq 1 -and $env:BHBuildSystem -eq 'AppVeyor') {
-    Deploy DeveloperBuild {
-        By AppVeyorModule {
-            FromSource $env:BHProjectName
-            To AppVeyor
-        }
-    }
-}
--- SwissKnife.build.ps1
+++ SwissKnife.build.ps1
@@ -95,7 +95,9 @@ Add-BuildTask BuildDocs {
 Add-BuildTask DeployPSGallery {
     # Publish the new version to the PowerShell Gallery
     try {
-        Invoke-PSDeploy -Verbose
+        nuget source Add -Name "GitHub" -Source "https://nuget.pkg.github.com/hjorslev/index.json" -UserName hjorslev -Password $env:GitHubKey
+        nuget pack
+        nuget push "SwissKnife.nupkg" -Source "GitHub"
         Write-Host -Object "$($env:BHProjectName) PowerShell Module version $($NewVersion) published to the PowerShell Gallery." -ForegroundColor Cyan
     } catch {
         # Sad panda; it broke
