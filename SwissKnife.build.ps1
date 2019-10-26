fd7891710641de016e3ee286e464181d6fcc38b6
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2019-10-27 01:28:30 +0200

    !deploy !ver:MINOR

--- SwissKnife.build.ps1
+++ SwissKnife.build.ps1
@@ -95,12 +95,12 @@ Add-BuildTask BuildDocs {
 Add-BuildTask DeployPSGallery {
     # Publish the new version to the PowerShell Gallery
     try {
-        Register-PSRepository -Name 'hjorslev' -SourceLocation 'https://nuget.hjorslev.com/upload' -PublishLocation 'https://nuget.hjorslev.com/api/v2/packages'
+        Register-PSRepository -Name 'hjorslev' -SourceLocation 'https://nuget.hjorslev.com/api/v2/packages' -PublishLocation 'https://nuget.hjorslev.com/api/v2/packages'
         Publish-Module -Path $env:BHModulePath -NuGetApiKey $env:NuGetApiKey -Repository 'hjorslev' -ErrorAction Stop
         Write-Host -Object "$($env:BHProjectName) PowerShell Module version $($NewVersion) published to the PowerShell Gallery." -ForegroundColor Cyan
     } catch {
         # Sad panda; it broke
-        Write-Warning -Message "Publishing update $($NewVersion) to the PowerShell Gallery failed."
+        Write-Warning -Message "Publishing update $($NewVersion) to failed."
         throw $_
     }
 }
