a9d84eea8fe4005a9ec04c7a7a03252984f50b45
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-02-28 23:57:27 +0100

    :white_check_mark: (Pester Test): Add test for Get-WAYFMetadata

---
+++ Tests/Public/Misc/Get-WAYFMetadata.Tests.ps1
@@ -0,0 +1,9 @@
+﻿Describe 'Get-WAYFMetadata' {
+    It 'Gets results from WAYF' {
+        { Get-WAYFMetadata } | Should -Not -Throw
+    }
+
+    It 'Finds schacHomeOrganization smk.dk' {
+        (Get-WAYFMetadata).schacHomeOrganization.Where{ $_ -eq 'cbs.dk' } | Should -BeExactly 'cbs.dk'
+    }
+}
